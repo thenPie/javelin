88c910be6fcde352fa81026e92f4b423b99efc33
thenPie <118397975+thenPie@users.noreply.github.com>
2023-05-04 23:46:47 +0300

    lect_1 42min

--- lectures/lect_1/src/Point2D.java
+++ lectures/lect_1/src/Point2D.java
@@ -1,22 +1,68 @@
 
 package src;
 
+/**
+ * Класс, создающий точку с данными координатами
+ * в двухмерном пространстве.
+ */
 public class Point2D {
 
     private Integer x;
     private Integer y;
 
-    Point2D(Integer x, Integer y) {
+    /**
+     * Это конструктор
+     * @param x - это координата X
+     * @param y - это координата Y
+     */
+    public Point2D(Integer x, Integer y) {
         this.x = x;
         this.y = y;
     }
 
+    // /**
+    //  * Устанавливает точку в двухмерном пространстве перегрузкой.
+    //  * @param x
+    //  */
+    // public Point2D(Integer x) {
+    //     this.x = x;
+    //     this.y = x;
+    // }
+
+    /**
+     * Устанавливает координату для X
+     * @param x
+     */
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    /**
+     * Устанавливает координату для Y
+     * @param y
+     */
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+    /**
+     * Возвращает координату точки X
+     * @return
+     */
     public Integer getX() {
         return x;
     }
 
+    /**
+     * Возвращает координату точки Y
+     * @return
+     */
     public Integer getY() {
         return y;
+    }
+
+    public static Double distance(Point2D a, Point2D b) {
+        return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
     }
 
     @Override
--- lectures/lect_1/src/main.java
+++ lectures/lect_1/src/main.java
@@ -3,17 +3,30 @@ package src;
 
 public class main {
 
-    static Double distance(Point2D a, Point2D b) {
-        return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2));
-    }
+    // /**
+    //  * Считывает дистанцию между двумя точками в двухмерном пространстве.
+    //  * @param a - первая точка.
+    //  * @param b - вторая точка.
+    //  * @return Дистанция между точками.
+    //  */
+    // static Double distance(Point2D a, Point2D b) {
+    //     return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2));
+    // }
 
     public static void main(String[] args) {
 
+        /**
+         Point2D a = new Point2D(3);
+         Point2D b = new Point2D(6);
+         
+         Double dis = distance(a, b);
+         System.out.println(dis);
+         */
+        
         Point2D a = new Point2D(0, 2);
         Point2D b = new Point2D(0, 10);
-
-        Double dist = distance(a, b);
-
+        
+        var dist = Point2D.distance(a, b);
         System.out.println(dist);
 
     }
