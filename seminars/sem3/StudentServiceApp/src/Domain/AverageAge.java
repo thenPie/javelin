28744eb0d720128e79bc8bd3ffd199f8ae6fb038
thenPie <118397975+thenPie@users.noreply.github.com>
2023-05-19 23:14:03 +0300

    fix

--- seminars/sem3/StudentServiceApp/src/Domain/AverageAge.java
+++ seminars/sem3/StudentServiceApp/src/Domain/AverageAge.java
@@ -11,23 +11,4 @@ public class AverageAge<E extends User> {
         avg = avg / people.size();
         return avg;
     }
-
-//    public static <T extends Person> void getAverageAge(List<T> list) {
-//        int sum = 0;
-//        for (T person : list) {
-//            sum += person.getAge();
-//        }
-//        sum /= list.size();
-//        System.out.printf("Average Age: %d\n", sum);
-//    }
-
-//    public static <E extends User> void getAverageAge(List<E> list) {
-//        int sum = 0;
-//        for (E person : list) {
-//            sum += person.getAge();
-//        }
-//        sum /= list.size();
-//        System.out.printf("Average Age: %d\n", sum);
-//    }
-
 }
