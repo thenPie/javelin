0a1b8b6bfd866db5ab2c05d86628ec53247a8fbe
thenPie <118397975+thenPie@users.noreply.github.com>
2023-05-26 00:49:17 +0300

    did 4

--- homework/lesson_6_oop_solid/fixed_solid/fourth/Square.java
+++ homework/lesson_6_oop_solid/fixed_solid/fourth/Square.java
@@ -5,5 +5,18 @@ public class Square extends Rectangle {
     public Square(int width) {
         super(width, width);
     }
+
+    @Override
+    public void setHeight(int height) {
+        super.height = height;
+        super.width = height;
+    }
+
+    @Override
+    public void setWidth(int width) {
+        super.height = width;
+        super.width = width;
+    }
+    
     
 }
--- homework/lesson_6_oop_solid/fixed_solid/fourth/main.java
+++ homework/lesson_6_oop_solid/fixed_solid/fourth/main.java
@@ -5,8 +5,28 @@ public class main {
         
         Rectangle rec = new Rectangle(2, 3);
         Square squ = new Square(3);
+        Rectangle rs = new Square(2);
 
         System.out.println(rec.toString());
         System.out.println(squ.toString());
+        System.out.println(rs.toString());
+
+        System.out.println(rec.area());
+        System.out.println(squ.area());
+        System.out.println(rs.area());
+
+        System.out.println();
+
+        rec.setWidth(5);
+        squ.setHeight(6);
+        rs.setHeight(7);
+        
+        System.out.println(rec.toString());
+        System.out.println(squ.toString());
+        System.out.println(rs.toString());
+
+        System.out.println(rec.area());
+        System.out.println(squ.area());
+        System.out.println(rs.area());
     }
 }
